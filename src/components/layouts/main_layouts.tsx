0bed78ef0ac97ec554efad69b56a148be7843590
M Zia <murtaza_zia@hotmail.co.uk>
2023-09-12 17:39:48 +0100

    introduced basic routing and tested manually. Still need to implement proper navLinks

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,17 @@
-export {}
+import { Outlet } from 'react-router'
+
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+
+const MainLayout = () => <>
+	<Header/>
+	<main className='p-6' style={{flex:'1'}}>
+		<Outlet />
+	 </main>
+     <Footer/>
+
+</>
+
+
+export default MainLayout
--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,25 @@
-export {}
-// what goes here??
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../home/home'
+import Help from '../help/help'
+import MainLayout from '../layouts/main_layouts'
+import Products from '../products/products'
+import Product from '../products/product/product'
+import NotFound from '../not_found/not_found'
+import About from '../about/about'
+
+const Router:React.FC = () => (
+	<Routes>
+		<Route path="/" element={<MainLayout />} >
+
+			<Route index element={<Home/>} />
+			<Route path='/help' element={<Help/>} />
+			<Route path='/about' element={<About/>} />
+			<Route path='/products' element={<Products/>} />
+			<Route path = '*' element={<NotFound/>} /> 
+
+		</Route>
+	</Routes>
+)
+
+export default Router
